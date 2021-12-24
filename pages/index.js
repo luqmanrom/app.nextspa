a5e0749ff0eea8480d46e15dba9eefc156134599
Luqman Rom <luqmanrom@gmail.com>
2021-12-25 01:51:35 +0800

    Init first SPA with Sanctum

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,47 @@
-export default function Home() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+import Head from 'next/head'
+import Link from 'next/link'
+import axios from '../lib/axios'
+
+
+export default function Home({offices}) {
+    return (
+        <>
+            <Head>
+                <title>ergodnc</title>
+            </Head>
+
+            {offices.map((office, index) => (
+                <div key={office.id} className={`flex ${index + 1 == offices.length ? '' : 'pb-10 mb-10 border-b'}`}>
+                    <div className="w-1/3 h-56 relative overflow-hidden rounded-lg">
+                        <img src={office.image} className="object-cover w-full h-full"></img>
+                    </div>
+
+                    <div className="w-full pl-14">
+                        <div className="flex justify-between items-center mb-6">
+                            <h1 className="text-2xl font-bold">{office.title}</h1>
+                            <span className="block font-semibold">${office.price_per_day / 100} per day</span>
+                        </div>
+                        <p className="leading-loose mb-5">
+                            {office.description}
+                        </p>
+                        <Link href={"/offices/" + office.id}>
+                            <a className="text-purple-600 font-bold">More details...</a>
+                        </Link>
+                    </div>
+                </div>
+            ))}
+        </>
+    )
+}
+
+export async function getStaticProps() {
+    const response = await axios.get('/api/offices');
+
+    console.log(response);
+
+    return {
+        props: {
+            offices: response.data
+        },
+    }
 }
